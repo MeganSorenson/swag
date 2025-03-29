831d8f0bea42ed75a7ec814741287e46589304f7
MeganSorenson <megansage01@gmail.com>
2025-03-29 13:45:21 -0700

    stitching working but havent tried with actual stitching

--- backend/image_stitcher.py
+++ backend/image_stitcher.py
@@ -1,11 +1,20 @@
 from stitching import Stitcher
+import os
+import cv2
 
 def stitch_images(folder_path):
     stitcher = Stitcher(detector="sift", confidence_threshold=0.2)
     
     # get all image path names ex: image.jpg from the folder_path
     image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png'))]
-    panorama = stitcher.stitch(image_paths)
+    print(image_paths)
+    if len(image_paths) < 2:
+        # save the single image called panorama.jpg (overwrite if exists)
+        panorama = cv2.imread(image_paths[0])
+    else:
+        panorama = stitcher.stitch(image_paths)
     
-    return panorama
+    cv2.imwrite("panoramas/panorama.jpg", panorama)
+    
+    return "panoramas/panorama.jpg"
     
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from cv_model import detect_faces
+from image_stitcher import stitch_images
 import os
 
 app = Flask(__name__)
@@ -8,18 +9,8 @@ CORS(app)
 
 @app.route('/detect', methods=['GET'])
 def detect():
-    # Use request.args for GET parameters instead of request.json
-    image_path = request.args.get('image', 'photo.jpg')
-    
-    # Check if image exists
-    if not os.path.exists(image_path):
-        return jsonify({
-            "success": False,
-            "error": f"Image not found: {image_path}"
-        }), 404
-    
-    # Process the image and get results
-    results = detect_faces(image_path, show_image=False)
+    panorama_path = stitch_images("images")
+    results = detect_faces(panorama_path)
     return jsonify(results)
 
 if __name__ == '__main__':
